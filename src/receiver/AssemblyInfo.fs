e48d37d105a1dee833f4b09f1cb99075fff3c879
Anton Zalyaldinov <a.zalyaldinov@systtech.ru>
2016-08-30 18:39:41 +0200

    feat: cmd middleware

---
+++ src/receiver/AssemblyInfo.fs
@@ -0,0 +1,21 @@
+﻿namespace Receiver.AssemblyInfo
+
+open System.Reflection
+open System.Runtime.InteropServices
+
+
+[<assembly: AssemblyTitle("receiver")>]
+[<assembly: AssemblyDescription("")>]
+[<assembly: AssemblyConfiguration("")>]
+[<assembly: AssemblyCompany("")>]
+[<assembly: AssemblyProduct("receiver")>]
+[<assembly: AssemblyCopyright("Copyright ©  2016")>]
+[<assembly: AssemblyTrademark("")>]
+[<assembly: AssemblyCulture("")>]
+[<assembly: ComVisible(false)>]
+[<assembly: Guid("C88A474C-E476-4778-A269-3B4B0D8A6B9C")>]
+[<assembly: AssemblyVersion("1.0.0.0")>]
+[<assembly: AssemblyFileVersion("1.0.0.0")>]
+
+do
+    ()
---
+++ src/receiver/Startup.fs
@@ -0,0 +1,78 @@
+﻿namespace Receiver
+
+open global.Owin
+open Microsoft.Owin
+
+open Newtonsoft.Json
+
+open System
+open System.Collections.Generic
+open System.Configuration
+open System.Diagnostics
+open System.IO
+open System.Security
+open System.Threading.Tasks
+open System.Net
+
+module Settings =    
+    
+    let user = ConfigurationManager.AppSettings.["user"]
+    
+    let password = 
+        ConfigurationManager.AppSettings.["password"].ToCharArray() 
+        |> Seq.fold (fun (s : SecureString) a -> s.AppendChar(a); s) (new SecureString())
+    
+    let handlers = 
+        File.ReadAllText(ConfigurationManager.AppSettings.["handlers"])
+        |> fun text -> JsonConvert.DeserializeObject<Dictionary<String, String>>(text)
+        |> Dictionary
+
+    let logFile() = Guid.NewGuid().ToString().Split('-').[0]
+
+type ReceiverMiddleware(next : OwinMiddleware) = 
+    inherit OwinMiddleware(next)
+
+    let output (arg: DataReceivedEventArgs) = 
+        if (isNull >> not) arg.Data 
+        then sprintf "<br/>%O:%s" DateTime.UtcNow arg.Data
+        else ""
+
+    override __.Invoke(context) = 
+        async { 
+            
+            let script = sprintf "/C \"%s\"" Settings.handlers.[context.Request.Path.Value]           
+            let startInfo = ProcessStartInfo(FileName = "cmd.exe", Arguments = script)
+            
+            startInfo.UserName <- Settings.user
+            startInfo.Password <- Settings.password
+            startInfo.CreateNoWindow <- true
+            startInfo.UseShellExecute <- false
+            startInfo.RedirectStandardOutput <- true
+            startInfo.RedirectStandardError <- true
+            context.Response.StatusCode <- int HttpStatusCode.OK
+            context.Response.ContentType <- "text/html"
+            use proc = new Process(StartInfo = startInfo)
+            proc.OutputDataReceived.Add(output >> context.Response.Write)
+            proc.ErrorDataReceived.Add(output >> context.Response.Write)
+                        
+            try                
+                proc.Start() |> ignore
+                proc.BeginOutputReadLine()
+                proc.BeginErrorReadLine()
+                do! async { proc.WaitForExit() }               
+            with e -> 
+                context.Response.Write(e.Message)              
+
+            return context       
+        }
+        |> Async.StartAsTask :> Task
+
+type Startup() =
+    
+
+    member __.Configuration(app:Owin.IAppBuilder) =           
+        app.Use<ReceiverMiddleware>() |> ignore
+
+        
+        
+
